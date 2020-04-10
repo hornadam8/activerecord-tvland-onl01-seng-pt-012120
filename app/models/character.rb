9582067d546091f7257081141b0e923f2c1645fb
Adam G Horn <hornadam8@gmail.com>
2020-04-10 03:42:33 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
   
-  def say_that_thing_you_always_say
+  def say_that_thing_you_say
     "#{self.name} always says #{self.catchprase}"
   end
 end
