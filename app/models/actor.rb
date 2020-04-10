eb5de1feccb85451aa2a9101fe84fdd6bcd73d86
Adam G Horn <hornadam8@gmail.com>
2020-04-10 02:58:10 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,15 @@
 class Actor < ActiveRecord::Base
   has_many :shows
   has_many :characters, through: :shows
+  
+  def full_name
+    fn = "#{self.first_name} #{self.last_name}"
+    fn
+  end
+  
+  def list_roles
+    roles = []
+    self.characters.map{|c| roles << 'c.name - c.shows.name'
+    roles
+  end
 end
